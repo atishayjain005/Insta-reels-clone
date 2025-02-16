0c02cebe5a555d6d46d2e1b8064a328eb273d2e1
Atishay Jain <Atishay@Apples-MacBook-Air.local>
2025-02-16 16:46:16 +0530

    incresed the width of reel in desktop

--- src/app/page.js
+++ src/app/page.js
@@ -8,18 +8,18 @@ const Home = () => {
   console.log(Videos)
   return (
     <div className="h-screen overflow-y-scroll snap-y snap-mandatory">
-      {Videos.map((video) => (
-        <div
-          key={video.id}
-          className="snap-start h-screen flex justify-center items-center"
-        >
-          {/* Phone-like container */}
-          <div className="w-full max-w-[400px] h-full p-4 rounded-lg overflow-hidden bg-black">
-            <VideoReel {...video} />
-          </div>
+    {Videos.map((video) => (
+      <div
+        key={video.id}
+        className="snap-start h-screen flex justify-center items-center"
+      >
+        {/* On mobile max width is 400px, on desktop we increase it */}
+        <div className="w-full max-w-[400px] md:max-w-[600px] h-full p-4 rounded-lg overflow-hidden bg-black">
+          <VideoReel {...video} />
         </div>
-      ))}
-    </div>
+      </div>
+    ))}
+  </div>
   );
 };
 
